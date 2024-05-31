818da5f62e3e7d38f7c3eea75d47cde48cb686fb
xuany <>
2024-05-31 14:08:07 +0800

    fix: toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import UnoCSS from 'unocss/astro'
 import sitemap from '@astrojs/sitemap';
+import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://exquisite-pie-2d2373.netlify.app',
+	markdown: {
+		remarkPlugins: [remarkToc]
+	},
 	integrations: [
 		mdx({ syntaxHighlight: 'shiki' }),
 		sitemap(),
