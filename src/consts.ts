65fda30126044514ab4e10c2fd8c1b42eed01d53
xuany <>
2024-07-07 22:10:51 +0800

    fix: update name

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import XSvg from "./assets/x.svg";
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jason Xu';
+export const SITE_TITLE = 'Jianxing Xu';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 
 export const Menus = [
